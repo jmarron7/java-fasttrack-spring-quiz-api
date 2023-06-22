150f07f4c48fe168492f039bef367cbc014154a6
Jesus Marron <jesusmarron7@gmail.com>
2023-06-22 15:04:17 -0500

    added other endpoints and service classes

--- src/main/java/com/cooksys/quiz_api/controllers/QuizController.java
+++ src/main/java/com/cooksys/quiz_api/controllers/QuizController.java
@@ -2,9 +2,13 @@ package com.cooksys.quiz_api.controllers;
 
 import java.util.List;
 
+import com.cooksys.quiz_api.dtos.QuestionRequestDto;
+import com.cooksys.quiz_api.dtos.QuestionResponseDto;
+import com.cooksys.quiz_api.dtos.QuizRequestDto;
 import com.cooksys.quiz_api.dtos.QuizResponseDto;
 import com.cooksys.quiz_api.services.QuizService;
 
+import javassist.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,5 +32,30 @@ public class QuizController {
   @ResponseStatus(HttpStatus.CREATED)
   public QuizResponseDto createQuiz(@RequestBody QuizRequestDto quizRequestDto) {
     return quizService.createQuiz();
+  }
+
+  @DeleteMapping("/{id}")
+  public QuizResponseDto deleteQuiz(@PathVariable Long id) {
+    return quizService.deleteQuiz(id);
+  }
+
+  @PatchMapping("/{id}/rename/{newName}")
+  public QuizResponseDto renameQuiz(@PathVariable Long id, @PathVariable String newName) throws NotFoundException {
+    return quizService.renameQuiz(id, newName);
+  }
+
+  @GetMapping("/{id}/random")
+  public QuestionResponseDto getQuizQuestion(@PathVariable Long id) throws NotFoundException {
+    return quizService.getQuizQuestion(id);
+  }
+
+  @PatchMapping("/{id}/add")
+  public QuizResponseDto addQuestion(@PathVariable Long id, @RequestBody QuestionRequestDto questionRequestDto) throws NotFoundException {
+    return quizService.addQuestion(id, questionRequestDto);
+  }
+
+  @DeleteMapping("/{id}/delete/{questionID}")
+  public QuestionResponseDto deleteQuestion(@PathVariable Long id, @PathVariable Long questionID) throws NotFoundException {
+    return quizService.deleteQuestion(id, questionID);
   }
 }
--- src/main/java/com/cooksys/quiz_api/services/QuizService.java
+++ src/main/java/com/cooksys/quiz_api/services/QuizService.java
@@ -2,10 +2,23 @@ package com.cooksys.quiz_api.services;
 
 import java.util.List;
 
+import com.cooksys.quiz_api.dtos.QuestionRequestDto;
+import com.cooksys.quiz_api.dtos.QuestionResponseDto;
 import com.cooksys.quiz_api.dtos.QuizResponseDto;
 
 public interface QuizService {
 
   List<QuizResponseDto> getAllQuizzes();
 
+  QuizResponseDto createQuiz();
+
+  QuizResponseDto deleteQuiz(Long id);
+
+  QuizResponseDto renameQuiz(Long id, String name);
+
+  QuestionResponseDto getQuizQuestion(Long id);
+
+  QuizResponseDto addQuestion(Long id, QuestionRequestDto questionRequestDto);
+
+  QuestionResponseDto deleteQuestion(Long id, Long questionID);
 }
